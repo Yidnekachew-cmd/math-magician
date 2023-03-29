8cefd970459a957e011ac4c796a2fea8c401d9b7
Yidnekachew Kassahun Kibret <yamiyekn@gmail.com>
2023-03-29 13:56:55 +0300

    Add eventes for the calculator.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import Keypads from './Keypads';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div>
-    <Keypads />
-  </div>
-);
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: '0',
+    next: null,
+    operation: null,
+  });
 
+  const handleClick = (e) => {
+    const targetText = e.target.textContent;
+    const newState = calculate(state, targetText);
+    setState(newState);
+  };
+  return (
+    <div>
+
+      <Keypads handleClick={handleClick} state={state} />
+    </div>
+  );
+};
 export default Calculator;
