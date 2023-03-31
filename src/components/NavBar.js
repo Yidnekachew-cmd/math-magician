0f37bcbede348788630325ad68f28fc30fdf4df0
Yidne <yamiyekn@gmail.com>
2023-03-31 11:40:03 +0300

    Fix linter.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,15 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => (
   <nav className="navBar">
-    <h1 >Math Magicians</h1>
-    <ul >
-      <li >
+    <h1>Math Magicians</h1>
+    <ul>
+      <li>
         <Link className="list-link" to="/">Home</Link>
       </li>
-      <li >
+      <li>
         <Link className="list-link" to="/Calculator">Calculator</Link>
       </li>
-      <li >
+      <li>
         <Link to="/Quote">Quote</Link>
       </li>
     </ul>
